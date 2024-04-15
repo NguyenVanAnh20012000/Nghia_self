6a1f39eaad83bd03baecb370baa4b7fe5802eed7
NguyenVanAnh20012000 <minhquanpham03122000@gmail.com>
2024-04-14 20:10:39 -0700

    handle test function better

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include "common/assert_handler.h"
 #include "drivers/mcu_init.h"
-SUPPRESS_UNUSED
 static void test_setup(void)
 {
     mcu_init();
 }
 int main(void)
 {
+    test_setup();
     ASSERT(0);
     return 0;
 }
