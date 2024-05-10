2e6caedee579d4760117441ba661fb327e967d85
NguyenVanAnh20012000 <minhquanpham03122000@gmail.com>
2024-05-09 23:50:18 -0700

    motor driver TB6612FNG

--- src/test/test.c
+++ src/test/test.c
@@ -8,6 +8,7 @@
 #include "drivers/uart.h"
 #include "drivers/ir_remote.h"
 #include "drivers/pwm.h"
+#include "drivers/tb6612fng.h"
 #include "external/printf/printf.h"
 #include "common/trace.h"
 #include "common/assert_handler.h"
@@ -179,7 +180,6 @@ static void test_ir_remote(void)
     }
 }
 SUPPRESS_UNUSED
-SUPPRESS_UNUSED
 static void test_pwm(void)
 {
     test_setup();
@@ -196,7 +196,32 @@ static void test_pwm(void)
         }
     }
 }
-
+SUPPRESS_UNUSED
+static void test_tb6612fng(void) {
+    test_setup();
+    trace_init();
+    tb6612fng_init();
+    const uint8_t duty_cycles[] = {100,100,100,100};
+    const tb6612fng_mode_e modes[] = {
+        TB6612FNG_MODE_FORWARD,
+        TB6612FNG_MODE_REVERSE,
+        TB6612FNG_MODE_FORWARD,
+        TB6612FNG_MODE_REVERSE,
+    };
+    while(1) {
+        for(uint8_t i = 0;i<ARRAY_SIZE(duty_cycles);i++) {
+            TRACE("Set mode %d and duty cycle %d", modes[i], duty_cycles[i]);
+            tb6612fng_set_mode(TB6612FNG_LEFT, modes[i]);
+            tb6612fng_set_mode(TB6612FNG_RIGHT, modes[i]);
+            tb6612fng_set_pwm(TB6612FNG_LEFT, duty_cycles[i]);
+            tb6612fng_set_pwm(TB6612FNG_RIGHT, duty_cycles[i]);
+            BUSY_WAIT_ms(3000);
+            tb6612fng_set_mode(TB6612FNG_LEFT,TB6612FNG_MODE_STOP);
+            tb6612fng_set_mode(TB6612FNG_RIGHT,TB6612FNG_MODE_STOP);
+            BUSY_WAIT_ms(1000);
+        }
+    }
+}
 
 int main () {
     TEST();
